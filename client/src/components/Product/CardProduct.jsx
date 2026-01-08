3579cf8a4d8b9c3ce1592b5331bfb76114d8187a
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-01-08 12:37:41 +0530

    update the app

--- client/src/components/Product/CardProduct.jsx
+++ client/src/components/Product/CardProduct.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+/*import React,{useState} from 'react'
 import { DisplayPriceInRupees } from '../../utils/DisplayPriceInRupees';
 import { Link } from 'react-router-dom';
 import { validURLConvert } from '../../utils/ValidUrlConverter';
@@ -55,3 +55,75 @@ const CardProduct = ({ data }) => {
 }
 
 export default CardProduct
+*/
+
+
+
+import React from "react";
+import { DisplayPriceInRupees } from "../../utils/DisplayPriceInRupees";
+import { Link } from "react-router-dom";
+import { validURLConvert } from "../../utils/ValidUrlConverter";
+import { PriceWithDiscount } from "../../utils/PriceWithDiscount";
+import AddToCartButton from "./AddToCartButton.jsx";
+
+const CardProduct = ({ data }) => {
+  const url = `/product/${validURLConvert(data.name)}-${data._id}`;
+
+  return (
+    <div className="border border-gray-200 rounded bg-white hover:bg-primary-100/20 min-w-[180px] lg:min-w-[210px] min-h-[340px] flex flex-col justify-between">
+      {/* Clickable area */}
+      <Link to={url} className="block">
+        {/* Image */}
+        <div className="h-32 lg:h-40 flex items-center justify-center bg-gray-50 rounded-t overflow-hidden">
+          <img
+            src={data?.image?.[0] || "/placeholder.png"}
+            alt={data?.name}
+            className="w-full h-full object-contain"
+            onError={(e) => {
+              e.currentTarget.src = "/placeholder.png";
+            }}
+          />
+        </div>
+
+        {/* Delivery time */}
+        <div className="px-2 mt-2">
+          <span className="inline-block px-2 py-0.5 text-xs text-green-600 bg-green-100 rounded-full">
+            10 min
+          </span>
+        </div>
+
+        {/* Name */}
+        <div className="px-2 mt-2 font-medium text-sm lg:text-base line-clamp-2">
+          {data.name}
+        </div>
+
+        {/* Unit */}
+        <div className="px-2 mt-1 text-sm text-gray-600">{data.unit}</div>
+      </Link>
+
+      {/* Price + Cart (NOT inside Link) */}
+      <div className="px-2 py-3 flex items-center justify-between">
+        <div>
+          <div className="font-semibold text-sm lg:text-base">
+            {DisplayPriceInRupees(
+              PriceWithDiscount(data?.price, data?.discount)
+            )}
+          </div>
+          {data?.discount > 0 && (
+            <p className="text-xs text-green-700">{data.discount}% OFF</p>
+          )}
+        </div>
+
+        <div>
+          {data.stock === 0 ? (
+            <p className="text-xs text-red-500">Out of stock</p>
+          ) : (
+            <AddToCartButton data={data} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardProduct;
